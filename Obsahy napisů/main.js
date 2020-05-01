8563cef74bb724939323524977ab3a4f35fc7fd7
JakubSilny <57211262+JakubSilny@users.noreply.github.com>
2020-05-01 22:45:28 +0200

    Add files via upload

---
+++ Obsahy napisů/main.js
@@ -0,0 +1,18 @@
+const init= function() {
+
+}
+const GenerateObsah = function()
+{
+  //let h1 = [];
+  //let allH1 = document.querySelectorAll("h1")
+  //document.getElementsByTagName("h1")[0].setAttribute("id", "H1");
+  //  console.log(allH1);
+  //  
+  //  for (let i = 0; i < h1.length; i++) {
+  //    
+  //      let CelyElement = h1[i]; //cely tag
+  //      console.log(CelyElement);
+  //    }
+         
+}
+document.addEventListener('DOMContentLoaded', init);
